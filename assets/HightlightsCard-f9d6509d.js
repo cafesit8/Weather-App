cb93cd5aeafbd7558a063cac8a4219d5c1f597ef
leliasgomer <leliasgomer@gmail.com>
2023-06-19 16:59:46 -0500

    Updates

---
+++ assets/HightlightsCard-f9d6509d.js
@@ -0,0 +1,1 @@
+import{j as e,a as t}from"./index-4bb42c68.js";function c({text:s,info:a,symbol:l}){return e("div",{className:"bg-[#1E213A] p-3 flex flex-col rounded-lg justify-center items-center",children:[t("h4",{className:"text-[18px]",children:s}),e("h5",{className:"text-[60px]",children:[a,t("span",{className:"text-[40px] text-white/50",children:l})]})]})}export{c as default};
